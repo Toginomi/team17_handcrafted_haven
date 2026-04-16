96f71f365c1f13e6c0dcb4b2af7881f8c40b4851
woodrow-snow <83348661+woodrow-snow@users.noreply.github.com>
2026-04-15 21:45:34 -0600

    Revert "Week7_Update"

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
   turbopack: {
-    root: process.cwd(),
-  },
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      }
-    ],
-  },
+  root: process.cwd(),
+},
 };
 
 export default nextConfig;
